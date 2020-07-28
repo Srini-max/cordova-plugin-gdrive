56dd0b0bdc3bfb0bd0928bb3ba27612b00c30d87
Srinivas <54628670+Srini-max@users.noreply.github.com>
2020-07-29 03:55:29 +0400

    Update googleDrive.js

--- www/googleDrive.js
+++ www/googleDrive.js
@@ -1,24 +1,32 @@
-var exec = require('cordova/exec');
+function GoogleDrive() {}
 
+GoogleDrive.prototype.downloadFile = function (destinationURL,fileid,successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "GoogleDrive", "downloadFile", [destinationURL,fileid]);
+};
 
-exports.signIn = function (successCallback, errorCallback) {
-    return new Promise(function (resolve, reject) {
-      exec(successCallback, errorCallback, "GoogleDrive", "signIn")
-    })
-}
+GoogleDrive.prototype.uploadFile = function (fpath,appfolder,successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "GoogleDrive", "uploadFile", [fpath,appfolder]);
+};
 
-exports.signOut = function (successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "GoogleDrive", "signOut");
-}
+GoogleDrive.prototype.fileList = function (appfolder,successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "GoogleDrive", "fileList", [appfolder]);
+};
 
+GoogleDrive.prototype.deleteFile = function (fileid,successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "GoogleDrive", "deleteFile", [fileid]);
+};
 
-exports.fileList = function ( appfolder,successCallback, errorCallback) {
-    return new Promise(function (resolve, reject) {
-      exec(successCallback, errorCallback, "GoogleDrive", "fileList", [appfolder])
-    })
-}
+GoogleDrive.prototype.requestSync = function(returnFiles,successCallback,errorCallback){
+    cordova.exec(successCallback,errorCallback,"GoogleDrive","requestSync",[returnFiles]);
+};
 
-exports.downloadFiles = function(downloadFiles,successCallback, errorCallback)
-{
-    exec(successCallback,errorCallback,"GoogleDrive", "downloadFiles",downloadFiles);
-}
+GoogleDrive.install = function () {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+
+    window.plugins.gdrive = new GoogleDrive();
+    return window.plugins.gdrive;
+};
+
+cordova.addConstructor(GoogleDrive.install);
